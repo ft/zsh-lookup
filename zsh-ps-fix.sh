20c3d140b02617ffbe5fa9c5a9fe3ca76f9c7cf3
Frank Terbeck <ft@bewatermyfriend.org>
2009-03-01 00:27:19 +0100

    Improve zsh-ps-fix.sh for gitfp()

--- zsh-ps-fix.sh
+++ zsh-ps-fix.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-for i in *.patch; do
-    (
-        printf '/^Subject:\n'
-        printf 's,\[PATCH \([0-9]\+/[0-9]\+\)\],PATCH: (\\1),\n'
-        printf 'w\nq\n'
-    ) | ed "$i"
-done
+# fixes [PATCH] to PATCH:
+#   and [PATCH m/n] to PATCH: (m/n)
+#
+# zsh-workers prefers PATCH: as its prefix.
+# gitfp() in my zsh configuration handles this automatically.
+
+[ -z "$1" ] && exit 1
+
+(
+    printf '/^Subject: \[\n'
+    printf 's,\[PATCH \([0-9]\+/[0-9]\+\)\],PATCH: (\\1),\n'
+    printf 'w\nq\n'
+) | ed "$1" > /dev/null 2>&1
+
+[ "$?" -eq 0 ] && exit 0
+
+(
+    printf '/^Subject: \[\n'
+    printf 's,\[PATCH\],PATCH:,\n'
+    printf 'w\nq\n'
+) | ed "$1" > /dev/null 2>&1
+
+exit 0
